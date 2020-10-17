70fff1918dbe9d82d312f5df9c154a8029f990b5
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-10-18 01:13:53 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan8/pecahan/IF110119028Latihan8Pecahan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan8.pecahan;
+
+/**
+ *
+ * @author RizalSihombing
+ */
+public class IF110119028Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
